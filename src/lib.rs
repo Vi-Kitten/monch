11ed89913ff4e7a601a9f3b9c5f2579f1f503d92
Vi <vi.kitten@proton.me>
2024-02-13 05:42:04 +0000

    moist code UwU (i mean it)

--- src/lib.rs
+++ src/lib.rs
@@ -62,7 +62,6 @@ pub macro apply {
 pub trait Parser<I, T, E = ParseError> where
     I: Iterator + Clone
 {
-
     fn parse(&self, iter: &mut I) -> Result<T, E>;
 
     fn discard(&self) -> impl Parser<I, (), E> {
@@ -93,8 +92,12 @@ pub trait Parser<I, T, E = ParseError> where
         }
     }
 
+    fn attempt_parse(&self, iter: &mut I) -> Result<T, E> {
+        self.attempt().parse(iter)
+    }
+
     // backtrack on success
-    fn scy(&self) -> impl Parser<I, T, E> {
+    fn scry(&self) -> impl Parser<I, T, E> {
         move |iter: &mut I| {
             let backup = iter.clone();
             let val = self.parse(iter)?;
@@ -103,11 +106,19 @@ pub trait Parser<I, T, E = ParseError> where
         }
     }
 
+    fn scrying_parse(&self, iter: &mut I) -> Result<T, E> {
+        self.scry().parse(iter)
+    }
+
     // always backtrack
     fn backtrack(&self) -> impl Parser<I, T, E> {
         move |iter: &mut I| {
             self.parse(&mut iter.clone())
         }
+    }
+
+    fn backtracking_parse(&self, iter: &mut I) -> Result<T, E> {
+        self.backtrack().parse(iter)
     }
 
     //* Value mapping
@@ -118,27 +129,65 @@ pub trait Parser<I, T, E = ParseError> where
         }
     }
 
-    // variants:
-    // scry_and...
-    // backtrack_and...
-
     fn and_then<U>(&self, f: &impl Fn(T) -> Result<U, E>) -> impl Parser<I, U, E> {
         move |iter: &mut I| {
             self.parse(iter).and_then(&f)
         }
     }
 
-    fn and_parse<U>(&self, p: &impl Parser<I, U, E>) -> impl Parser<I, U, E> {
+    fn scry_and_then<U>(&self, f: &impl Fn(T) -> Result<U, E>) -> impl Parser<I, U, E> {
+        move |iter: &mut I| {
+            self.scrying_parse(iter).and_then(&f)
+        }
+    }
+
+    fn backtrack_and_then<U>(&self, f: &impl Fn(T) -> Result<U, E>) -> impl Parser<I, U, E> {
+        move |iter: &mut I| {
+            self.backtracking_parse(iter).and_then(&f)
+        }
+    }
+
+    fn and_combine<U>(&self, p: &impl Parser<I, U, E>) -> impl Parser<I, U, E> {
         move |iter: &mut I| {
             self.parse(iter).and_then(|_| p.parse(iter))
         }
     }
 
-    fn and_then_parse<U, P>(&self, f: &impl Fn(T) -> P) -> impl Parser<I, U, E> where
+    fn scry_and_combine<U>(&self, p: &impl Parser<I, U, E>) -> impl Parser<I, U, E> {
+        move |iter: &mut I| {
+            self.scrying_parse(iter).and_then(|_| p.parse(iter))
+        }
+    }
+
+    fn backtrack_and_combine<U>(&self, p: &impl Parser<I, U, E>) -> impl Parser<I, U, E> {
+        move |iter: &mut I| {
+            self.backtracking_parse(iter).and_then(|_| p.parse(iter))
+        }
+    }
+
+    fn and_then_combine<U, P>(&self, f: &impl Fn(T) -> P) -> impl Parser<I, U, E> where
         P: Parser<I, U, E>
     {
         move |iter: &mut I| {
             let t = self.parse(iter)?;
+            f(t).parse(iter)
+        }
+    }
+
+    fn scry_and_then_combine<U, P>(&self, f: &impl Fn(T) -> P) -> impl Parser<I, U, E> where
+        P: Parser<I, U, E>
+    {
+        move |iter: &mut I| {
+            let t = self.scrying_parse(iter)?;
+            f(t).parse(iter)
+        }
+    }
+
+    fn backtrack_and_then_combine<U, P>(&self, f: &impl Fn(T) -> P) -> impl Parser<I, U, E> where
+        P: Parser<I, U, E>
+    {
+        move |iter: &mut I| {
+            let t = self.backtracking_parse(iter)?;
             f(t).parse(iter)
         }
     }
@@ -161,13 +210,37 @@ pub trait Parser<I, T, E = ParseError> where
         }
     }
 
-    fn or_parse<F>(&self, p: &impl Parser<I, T, F>) -> impl Parser<I, T, F> {
+    fn attempt_or_else<F>(&self, o: &impl Fn(E) -> Result<T, F>) -> impl Parser<I, T, F> {
+        move |iter: &mut I| {
+            self.attempt_parse(iter).or_else(&o)
+        }
+    }
+
+    fn backtrack_or_else<F>(&self, o: &impl Fn(E) -> Result<T, F>) -> impl Parser<I, T, F> {
+        move |iter: &mut I| {
+            self.backtracking_parse(iter).or_else(&o)
+        }
+    }
+
+    fn or_combine<F>(&self, p: &impl Parser<I, T, F>) -> impl Parser<I, T, F> {
         move |iter: &mut I| {
             self.parse(iter).or_else(|_| p.parse(iter))
         }
     }
 
-    fn or_else_parse<F, P>(&self, o: &impl Fn(E) -> P) -> impl Parser<I, T, F> where
+    fn attempt_or_combine<F>(&self, p: &impl Parser<I, T, F>) -> impl Parser<I, T, F> {
+        move |iter: &mut I| {
+            self.attempt_parse(iter).or_else(|_| p.parse(iter))
+        }
+    }
+
+    fn backtrack_or_combine<F>(&self, p: &impl Parser<I, T, F>) -> impl Parser<I, T, F> {
+        move |iter: &mut I| {
+            self.backtracking_parse(iter).or_else(|_| p.parse(iter))
+        }
+    }
+
+    fn or_else_combine<F, P>(&self, o: &impl Fn(E) -> P) -> impl Parser<I, T, F> where
         P: Parser<I, T, F>
     {
         move |iter: &mut I| {
@@ -177,12 +250,43 @@ pub trait Parser<I, T, E = ParseError> where
         }
     }
 
-    //* Combinators
+    fn attempt_or_else_combine<F, P>(&self, o: &impl Fn(E) -> P) -> impl Parser<I, T, F> where
+        P: Parser<I, T, F>
+    {
+        move |iter: &mut I| {
+            self.attempt_parse(iter).or_else(|e| {
+                o(e).parse(iter)
+            })
+        }
+    }
+
+    fn backtrack_or_else_combine<F, P>(&self, o: &impl Fn(E) -> P) -> impl Parser<I, T, F> where
+        P: Parser<I, T, F>
+    {
+        move |iter: &mut I| {
+            self.backtracking_parse(iter).or_else(|e| {
+                o(e).parse(iter)
+            })
+        }
+    }
+
+    //* Vector Combinators
 
     fn many<F>(&self) -> impl Parser<I, Vec<T>, F> {
         move |iter: &mut I| {
             let mut values = vec![];
             while let Ok(val) = self.parse(iter) {
+                values.push(val)
+            }
+            Ok(values)
+        }
+    }
+
+    fn attempt_many<F>(&self) -> impl Parser<I, Vec<T>, F> {
+        let p = self.attempt();
+        move |iter: &mut I| {
+            let mut values = vec![];
+            while let Ok(val) = p.parse(iter) {
                 values.push(val)
             }
             Ok(values)
@@ -199,11 +303,37 @@ pub trait Parser<I, T, E = ParseError> where
         }
     }
 
+    fn attempt_some(&self) -> impl Parser<I, Vec<T>, E> {
+        let p = self.attempt();
+        move |iter: &mut I| {
+            let val = p.parse(iter)?;
+            p.many().parse(iter).map(|mut values| {
+                values.insert(0, val);
+                values
+            })
+        }
+    }
+
     fn least_until<U, F>(&self, end: &impl Parser<I, U, F>) -> impl Parser<I, (Vec<T>, U), F> {
         move |iter: &mut I| {
             let mut values = vec![];
             let u = loop {
                 match end.parse(iter).or_else(|e| Err(self.parse(iter).map_err(|_| e))) {
+                    Ok(u) => break Ok(u),
+                    Err(Ok(val)) => values.push(val),
+                    Err(Err(e)) => break Err(e),
+                }
+            }?;
+            Ok((values, u))
+        }
+    }
+
+    fn attempt_least_until<U, F>(&self, end: &impl Parser<I, U, F>) -> impl Parser<I, (Vec<T>, U), F> {
+        let p = self.attempt();
+        move |iter: &mut I| {
+            let mut values = vec![];
+            let u = loop {
+                match end.parse(iter).or_else(|e| Err(p.parse(iter).map_err(|_| e))) {
                     Ok(u) => break Ok(u),
                     Err(Ok(val)) => values.push(val),
                     Err(Err(e)) => break Err(e),
@@ -244,11 +374,39 @@ pub trait Parser<I, T, E = ParseError> where
         }
     }
 
-    //* Error recovery
+    fn attempt_most_until<U, F>(&self, end: &impl Parser<I, U, F>) -> impl Parser<I, (Vec<T>, U), E> {
+        let p = self.attempt();
+        move |iter: &mut I| {
+            let mut stack = vec![iter.clone()];
+            let mut values = vec![];
+            let e = loop {
+                let mut child = stack.last().unwrap().clone();
+                let res = p.parse(&mut child);
+                stack.push(child);
+                match res {
+                    Ok(val) => values.push(val),
+                    Err(e) => break e,
+                }
+            };
+            loop {
+                let mut parent = stack.pop().unwrap();
+                match end.parse(&mut parent) {
+                    Ok(u) => {
+                        *iter = parent;
+                        break Ok((values, u))
+                    },
+                    Err(_) => {
+                        if let None = values.pop() {
+                            *iter = parent;
+                            break Err(e)
+                        }
+                    },
+                }
+            }
+        }
+    }
 
-    // variants:
-    // scry_then_continue_with
-    // backtrack_then_continue_with
+    //* Error recovery
 
     fn continue_with<F>(&self, p: &impl Parser<I, (), F>) -> impl Parser<I, Result<T, E>, F> {
         move |iter: &mut I| {
@@ -258,13 +416,43 @@ pub trait Parser<I, T, E = ParseError> where
         }
     }
 
-    // variants:
-    // attempt_then_recover_with
-    // backtrack_then_recover_with
+    fn scry_then_continue_with<F>(&self, p: &impl Parser<I, (), F>) -> impl Parser<I, Result<T, E>, F> {
+        move |iter: &mut I| {
+            let res = self.scrying_parse(iter);
+            p.parse(iter)?;
+            Ok(res)
+        }
+    }
+
+    fn backtrack_then_continue_with<F>(&self, p: &impl Parser<I, (), F>) -> impl Parser<I, Result<T, E>, F> {
+        move |iter: &mut I| {
+            let res = self.backtracking_parse(iter);
+            p.parse(iter)?;
+            Ok(res)
+        }
+    }
 
     fn recover_with<F>(&self, p: &impl Parser<I, (), F>) -> impl Parser<I, Result<T, E>, F> {
         move |iter: &mut I| {
             match self.parse(iter) {
+                Ok(res) => Ok(Ok(res)),
+                Err(e) => p.parse(iter).map(|_| Err(e)),
+            }
+        }
+    }
+
+    fn attempt_then_recover_with<F>(&self, p: &impl Parser<I, (), F>) -> impl Parser<I, Result<T, E>, F> {
+        move |iter: &mut I| {
+            match self.attempt_parse(iter) {
+                Ok(res) => Ok(Ok(res)),
+                Err(e) => p.parse(iter).map(|_| Err(e)),
+            }
+        }
+    }
+
+    fn backtrack_then_recover_with<F>(&self, p: &impl Parser<I, (), F>) -> impl Parser<I, Result<T, E>, F> {
+        move |iter: &mut I| {
+            match self.backtracking_parse(iter) {
                 Ok(res) => Ok(Ok(res)),
                 Err(e) => p.parse(iter).map(|_| Err(e)),
             }
@@ -278,5 +466,4 @@ pub trait Parser<I, T, E = ParseError> where
             self.parse(iter)?.into()
         }
     }
-
 }
