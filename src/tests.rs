58101c73b7d3ef84f4f687b0fe2760ffbab9173f
Vi <vi.kitten@proton.me>
2024-02-19 15:13:07 +0000

    first version fully tested

--- src/tests.rs
+++ src/tests.rs
@@ -67,7 +67,7 @@ fn test_parse() {
 fn test_parse_ok() {
     let mut iter = "".chars();
     assert_eq!(
-        parse_ok::<_, _,!>("abc".into())
+        parse_ok::<_, _, !>("abc".into())
         .parse(&mut iter),
         Ok("abc")
     );
@@ -885,44 +885,158 @@ fn test_attempt_most_until() {
 
 //* Error recovery
 
-#[ignore]
 #[test]
 fn test_continue_with() {
-    todo!()
+    let mut iter = "abcdefghijkl".chars();
+    assert_eq!(
+        expect("abc", "test_failure_0")
+        .continue_with(expect("def", "test_failure_1").discard())
+        .parse(&mut iter),
+        Ok(Ok("abc".into()))
+    );
+    assert_eq!(
+        expect("def", "err")
+        .continue_with(expect("jkl", "test_failure").discard())
+        .parse(&mut iter),
+        Ok(Err("err".into()))
+    );
+    assert_eq!(
+        expect_end("test_failure")
+        .parse(&mut iter),
+        Ok(())
+    )
 }
 
-#[ignore]
 #[test]
 fn test_scry_then_continue_with() {
-    todo!()
+    let mut iter = "abcdefghijkl".chars();
+    assert_eq!(
+        expect("abc", "test_failure_0")
+        .scry_then_continue_with(expect("abcdef", "test_failure_1").discard())
+        .parse(&mut iter),
+        Ok(Ok("abc".into()))
+    );
+    assert_eq!(
+        expect("def", "err")
+        .scry_then_continue_with(expect("jkl", "test_failure").discard())
+        .parse(&mut iter),
+        Ok(Err("err".into()))
+    );
+    assert_eq!(
+        expect_end("test_failure")
+        .parse(&mut iter),
+        Ok(())
+    )
 }
 
-#[ignore]
 #[test]
 fn test_backtrack_then_continue_with() {
-    todo!()
+    let mut iter = "abcdefghijkl".chars();
+    assert_eq!(
+        expect("abc", "test_failure_0")
+        .backtrack_then_continue_with(expect("abcdef", "test_failure_1").discard())
+        .parse(&mut iter),
+        Ok(Ok("abc".into()))
+    );
+    assert_eq!(
+        expect("def", "err")
+        .backtrack_then_continue_with(expect("ghijkl", "test_failure").discard())
+        .parse(&mut iter),
+        Ok(Err("err".into()))
+    );
+    assert_eq!(
+        expect_end("test_failure")
+        .parse(&mut iter),
+        Ok(())
+    )
 }
 
-#[ignore]
 #[test]
 fn test_recover_with() {
-    todo!()
+    let mut iter = "abcdefghi".chars();
+    assert_eq!(
+        expect("abc", "test_failure_0")
+        .recover_with(expect("def", "test_failure_1").discard())
+        .parse(&mut iter),
+        Ok(Ok("abc".into()))
+    );
+    assert_eq!(
+        expect("abc", "err")
+        .recover_with(expect("ghi", "test_failure").discard())
+        .parse(&mut iter),
+        Ok(Err("err".into()))
+    );
+    assert_eq!(
+        expect_end("test_failure")
+        .parse(&mut iter),
+        Ok(())
+    )
 }
 
-#[ignore]
 #[test]
 fn test_attempt_then_recover_with() {
-    todo!()
+    let mut iter = "abcdefghi".chars();
+    assert_eq!(
+        expect("abc", "test_failure_0")
+        .attempt_then_recover_with(expect("def", "test_failure_1").discard())
+        .parse(&mut iter),
+        Ok(Ok("abc".into()))
+    );
+    assert_eq!(
+        expect("abc", "err")
+        .attempt_then_recover_with(expect("defghi", "test_failure").discard())
+        .parse(&mut iter),
+        Ok(Err("err".into()))
+    );
+    assert_eq!(
+        expect_end("test_failure")
+        .parse(&mut iter),
+        Ok(())
+    )
 }
 
-#[ignore]
 #[test]
 fn test_backtrack_then_recover_with() {
-    todo!()
+    let mut iter = "abcdef".chars();
+    assert_eq!(
+        expect("abc", "test_failure_0")
+        .backtrack_then_recover_with(expect("def", "test_failure_1").discard())
+        .parse(&mut iter),
+        Ok(Ok("abc".into()))
+    );
+    assert_eq!(
+        expect("abcghi", "err")
+        .backtrack_then_recover_with(expect("abcdef", "test_failure").discard())
+        .parse(&mut iter),
+        Ok(Err("err".into()))
+    );
+    assert_eq!(
+        expect_end("test_failure")
+        .parse(&mut iter),
+        Ok(())
+    )
 }
 
-#[ignore]
 #[test]
 fn test_absorb_err() {
-    todo!()
+    let mut iter = "abcdef".chars();
+    assert_eq!(
+        expect("abc", "test_failure")
+        .map(Result::Ok)
+        .absorb_err()
+        .parse(&mut iter),
+        Ok("abc".into())
+    );
+    assert_eq!(
+        expect("def", "test_failure")
+        .map(|_| Err("err".into()))
+        .absorb_err::<!>()
+        .parse(&mut iter),
+        Err("err".into())
+    );
+    assert_eq!(
+        expect_end("test_failure")
+        .parse(&mut iter),
+        Ok(())
+    )
 }
